bb192dde4186535cfc71d73ffb2ea24a0584412c
Ubuntu <ubuntu@ip-172-31-18-24.us-west-1.compute.internal>
2020-09-07 00:41:32 +0000

    first server code

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = require('./router');
+const port = 9000;
+const app = express(
+);
+
+app.listen(port, () =>
+	console.log(`Example app listening on port port 9000!`),
+);
+app.get('/amar', function(req,res) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN');
+	res.send("hello");
+});
+
+app.get('/sydney', function(req,res) {
+	const rishigod = `hello sydney ${req.query.test}`;
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, X-XSRF-TOKEN');
+	res.send(rishigod);
+});
