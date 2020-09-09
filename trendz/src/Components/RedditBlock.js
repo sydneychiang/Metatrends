ec5c86ad3d5828a9f2ada69cf6f763c8839b74be
Sydney Chiang <sjchiang@uci.edu>
2020-09-09 16:29:58 -0400

    Reddit component formatted with img

--- trendz/src/Components/RedditBlock.js
+++ trendz/src/Components/RedditBlock.js
@@ -6,14 +6,20 @@ import Divider from '@material-ui/core/Divider';
 function RedditBlock() {
     return (
         <div id="redditBlock">
+            <img src={liam} align="left"/> 
+
             <span className="reddit">Reddit</span>
             <span className="trendingNum">#10 Trending</span>
-
+            
             <span className="subreddit">r/MovieDetails</span>
             <span className="description">When Liam Neeson was asked to play the antagonist in A Million Ways
-             to die in the West (2014) (directed by Seth McFarlane) he accepted due to a scene in Family Guy 
-             where Peter says “Imagine Liam Neeson in a Western! Ha! With that funny accent of his!”</span>
-            <img src={liam} /> 
+            to die in the West (2014) (directed by Seth McFarlane) he accepted due to a scene in Family Guy 
+            where Peter says “Imagine Liam Neeson in a Western! Ha! With that funny accent of his!”</span>
+        
+
+
+
+            
             <Divider variant="middle" style={{marginBottom: '15px'}} />
         </div>
     )
